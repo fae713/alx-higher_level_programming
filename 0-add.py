90e665e3803ef76bf0cde40657a61bc51a46572c
fae713 <favourmichael47@gmail.com>
2023-10-06 01:08:36 -0700

    task 0

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+
+    if __name__ == "__main__":
+    """Ptint the sum a and b"""
+
+    from add_0 import add
+    a = 1
+    b = 1
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
