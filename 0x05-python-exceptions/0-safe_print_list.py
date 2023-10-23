9143abc2f49b0c3ff5981649483b9cf59c334ed3
fae713 <favourmichael47@gmail.com>
2023-10-23 05:49:46 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0 
+    
+    try:
+        for Xelements in range(x):
+            print(my_list[Xelements], end=" ")
+            count += 1
+    except IndexError as ex:
+        pass
+    
+    print()
+    
+    return count
