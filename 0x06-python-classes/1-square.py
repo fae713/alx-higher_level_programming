68b1e0be576551f29f0d3e98d13ab370e9a233f9
fae713 <favourmichael47@gmail.com>
2023-10-30 06:59:51 -0700

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Defines a class for square"""
+"""This is the sqaure module"""
 
 
 class Square:
-    """This class defines a square private instance"""
-
+    """This class defines a sqaure instance"""
 
     def __init__(self, size=None):
-        """Create a new instances of the square"""
+        """This method defines the private size of the sqaure object"""
 
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""This runs the python interpreter"""
+"""Defines a class Square"""
 
 
 class Square:
-    """This defines a class called Square"""
-
-
+    """Class that defines properties of square by: (based on 1-square.py)."""
     def __init__(self, size=0):
-        """This defines an instance attributes"""
-
+        """Creates new instances of square."""
         self.__size = size
-        """This is a private instance of the square size"""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
