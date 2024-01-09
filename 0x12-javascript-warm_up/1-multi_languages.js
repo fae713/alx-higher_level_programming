7a49948ac6d337d892e02549f98dc6430394a432
fae713 <favourmichael47@gmail.com>
2024-01-09 08:07:57 -0800

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function myFunc () {
+  const firstline = 'C is fun';
+  const secondline = 'Python is cool';
+  const thirdline = 'JavaScript is amazing';
+
+  console.log(firstline);
+  console.log(secondline);
+  console.log(thirdline);
+}
+
+myFunc();
