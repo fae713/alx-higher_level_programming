0047a1e69b4654bb0b9f5bf77e30d207b6924825
fae713 <favourmichael47@gmail.com>
2024-01-09 07:33:19 -0800

    task 0

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Create a constant variable called myVar
+const myVar = "JavaScript is amazing";
+
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+This is the readme file for javaScript warm up
