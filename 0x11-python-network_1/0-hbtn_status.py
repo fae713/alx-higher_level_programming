2633d1cad2f3340cb12433a215115b0ee4073458
fae713 <favourmichael47@gmail.com>
2024-01-26 07:23:19 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
  a Python script that fetches https://alx-intranet.hbtn.io/status
+ using urllib
 
 """
 
-import urllib.request
-
-url = "https://alx-intranet.hbtn.io/status"
-response = urllib.request.urlopen(url)
-body_content = response.read()
-print(f"""
-- type: {type(body_content)}
-- content: {body_content}
-- utf8 content: {body_content.decode("utf-8")}
-""")
+if __name__ == "__main____":
+    import urllib.request
+    url = "https://alx-intranet.hbtn.io/status"
+    response = urllib.request.urlopen(url)
+    read_body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(read_body)))
+    print("\t- content: {}".format(read_body))
+    print("\t- utf8 content: {}".format(read_body.decode('utf-8')))
